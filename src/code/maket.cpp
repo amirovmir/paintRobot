bcb6efdb5b98c433c4d46b158570f6562a75d712
amirovmir <78919537+amirovmir@users.noreply.github.com>
2022-04-23 13:44:04 +0300

    version 2.0

--- src/code/maket.cpp
+++ src/code/maket.cpp
@@ -1,149 +1,118 @@
 #include <iostream>
-
+#include<sstream>
+#include<string>
 using namespace std;
-
-enum EState 
+class  CommandStrategy
 {
-    Off,
+public:
+    virtual string getCommand() const = 0;
+};
+class  ConsoleCommandStrategy  : public CommandStrategy 
+{
+public:
+    virtual string getCommand() const override 
+    {
+      string cmd;
+      cin >> cmd;
+      return cmd;
+    }
+};
+
+class NetCommandStrategy : public CommandStrategy 
+{
+  virtual string getCommand() const override 
+  {
+    string cmd;
+    //cmd = receive();
+    return cmd;
+  }
+};
+
+
+class  OnTestCommandStrategy  : public CommandStrategy 
+{
+public:
+  virtual string getCommand() const override 
+  {
+    return string("On");
+  }
+};
+enum class EState 
+{
     Wait_cmd,
-    Reverse,
-    Move_straight,
-    Clean,
-    Error
+	Reverse,
+	EOn,
+    EOff
 };
 
 class Robot
 {
-    EState state;
-    double angle;
-    double distance;
-    GetCommand* robotStrategy;
+  EState state;
+  CommandStrategy* pcommandprocessor;
 public:
-    Robot(GetCommand* _strategy): robotStrategy(_strategy) {};
-    ~Robot()
-    { 
-        delete robotStrategy;
-    };
-    EState getState();
-    void setState(EState nState);
-    void processEvent(EState state);
-    //void getCoordinates();
-    void rotate(double angle);
-    void moveStraight(double distance);
-    void clean();
-    void getCmd();
+  void processEvent(const string& command, EState& state);
+  void getCoordinates();
+  void reverse(double angle);
+  void moveStraight(double distance);
+  void clean();
+  int R(const string& com);
+  Robot(CommandStrategy* pcommandprocessor) {}
+  string getCommand() 
+  {
+    string cmd;
+    cin >> cmd;
+    return cmd;
+  }
 };
 
-void Robot::getCmd() 
+void Robot::reverse(double angle)
 {
-    robotStrategy->getEvent();
+  cout << "Robot turned to " << angle << " degree" << endl;
+};
+void Robot::moveStraight(double distance)
+{
+  cout << "Robot moved straight to " << distance << endl;
+};
+
+void Robot::clean()
+{
+  cout << "Robot cleaned the area" << endl;
+};
+int Robot::R(const string& com)
+{
+	if(com == "Reverse")
+  {
+		return 1;
+	} else if(com == "Wait_cmd")
+  {
+		return 2;
+	}
+}
+void Robot::processEvent(const string& command, EState& state){
+	string com;
+	cin >> com;
+	switch (R(com))
+	{
+	  case 2:
+	    Wait_cmdEventProcessing(command);
+	    break;
+    case 1:
+	    double angl;
+	    cin >> angl;
+	    Robot::reverse(angl);
+	    state = EState::Wait_cmd;
+      break;
+	}
 }
 
-void Robot::rotate(double angle) 
+int main()
 {
-    cout << "Robot turned to " << angle << " degree\n";
-};
-
-void Robot::moveStraight(double distance) 
-{
-    cout << "Robot moved straight to " << distance << endl;
-};
-
-void Robot::clean() 
-{
-    cout << "Robot cleaned the area\n";
-};
-
-void Robot::processEvent(EState state) 
-{
-    while (state != Off) 
+    Robot paintRobot (new NetCommandStrategy());
+   paintRobot.state = EState::Wait_cmd;
+    while (paintRobot.state == EState::Wait_cmd)
     {
-        Robot::getCmd();
-        switch (state) 
-        {
-            case Wait_cmd:  
-                Robot::getCmd();
-                break;
-
-            case Reverse:
-                double angl;
-                cin >> angl;
-                Robot::rotate(angl);
-                state = Wait_cmd;
-                break;
-
-            case Move_straight:
-                double dist;
-                cin >> dist;
-                Robot::moveStraight(dist);
-                state = Wait_cmd;
-                break;
-
-            case Clean:
-                Robot::clean();
-                state = Wait_cmd;
-                break;
-
-            case Error:
-                state = Off;
-                break;
-
-            default:
-                state = Wait_cmd;
-                break;
-        }
-    }
-};
-
-EState Robot::getState() 
-{
-    return this->state;
-};
-
-void Robot::setState(EState nState) 
-{
-    state = nState;
-};
-
-class GetCommand 
-{
-public:
-virtual ~GetCommand() {};
-    virtual void getEvent() = 0;
-};
-
-class NetworkStrategy : public GetCommand 
-{
-
-};
-
-class KeyboardStrategy : public GetCommand 
-{
-    void getEvent() override
-    {
-        int str;
-        cin >> str;
-        if (str >= Off && str <= Error) 
-        {
-            EState state = static_cast<EState>(str);
-        }
-        else
-        {
-            cout << "Wrong command!";
-        }
-
-    }
-};
-
-int main() 
-{
-    Robot paintRobot = new Robot(new KeyboardStrategy);
-    paintRobot.setState(Wait_cmd);
-    
-    while (paintRobot.getState() != Off) 
-    {
-        paintRobot.processEvent(paintRobot.getState());
-    }
-
+	  string cmd = paintRobot.getCommand();
+	  paintRobot.processEvent(cmd, paintRobot.state);
+	}
     return 0;
 }
