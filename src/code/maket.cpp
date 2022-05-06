67a92f4ab0671245f76ebf3f02ca59ca121f9cb2
amirovmir <78919537+amirovmir@users.noreply.github.com>
2022-05-06 23:20:04 +0300

    Pattern Strategy works now

--- src/code/maket.cpp
+++ src/code/maket.cpp
@@ -1,118 +1,201 @@
 #include <iostream>
 #include<sstream>
 #include<string>
+
+#define WAIT_CMD 1
+#define REVERSE  2
+#define CLEAN    3
+#define ON       4
+#define OFF      5
+
 using namespace std;
+
 class  CommandStrategy
 {
 public:
-    virtual string getCommand() const = 0;
+	virtual string getCommand() const = 0;
+	virtual ~CommandStrategy() {}
 };
-class  ConsoleCommandStrategy  : public CommandStrategy 
+
+class  ConsoleCommandStrategy : public CommandStrategy
 {
 public:
-    virtual string getCommand() const override 
-    {
-      string cmd;
-      cin >> cmd;
-      return cmd;
-    }
+	virtual string getCommand() const override
+	{
+		string cmd;
+		cin >> cmd;
+		return cmd;
+	}
 };
 
-class NetCommandStrategy : public CommandStrategy 
+class NetCommandStrategy : public CommandStrategy
 {
-  virtual string getCommand() const override 
-  {
-    string cmd;
-    //cmd = receive();
-    return cmd;
-  }
+	virtual string getCommand() const override
+	{
+		string cmd;
+		//cmd = receive();
+		return cmd;
+	}
 };
 
-
-class  OnTestCommandStrategy  : public CommandStrategy 
+class  OnTestCommandStrategy : public CommandStrategy
 {
 public:
-  virtual string getCommand() const override 
-  {
-    return string("On");
-  }
+	virtual string getCommand() const override
+	{
+		return string("On");
+	}
 };
-enum class EState 
-{
-    Wait_cmd,
-	Reverse,
-	EOn,
-    EOff
-};
+
+enum class EState {	Wait_cmd, Reverse, EOn,	EOff };
 
 class Robot
 {
-  EState state;
-  CommandStrategy* pcommandprocessor;
+	double angle;
+	double distance;
+	EState state;
+	CommandStrategy *commandprocessor;	
 public:
-  void processEvent(const string& command, EState& state);
-  void getCoordinates();
-  void reverse(double angle);
-  void moveStraight(double distance);
-  void clean();
-  int R(const string& com);
-  Robot(CommandStrategy* pcommandprocessor) {}
-  string getCommand() 
-  {
-    string cmd;
-    cin >> cmd;
-    return cmd;
-  }
+	Robot(CommandStrategy *pcommandprocessor) : commandprocessor(pcommandprocessor) {};
+	~Robot()
+	{
+		delete commandprocessor;
+	}
+	void processEvent(const string& command);
+	void Wait_cmdEventProcessing(const string& cmd);
+
+	double getAngle();
+	void setAngle(double n_angle);
+	double getDistance();
+	void setDistance(double n_distance);
+	EState getState();
+	void setState(EState n_state);
+	void reverse(double n_angle);
+	void moveStraight(double n_distance);
+	void clean();
+
+	int R(const string& com);
+	string getCommand()
+	{
+		return commandprocessor->getCommand();
+	}
 };
 
-void Robot::reverse(double angle)
+double Robot::getAngle()
 {
-  cout << "Robot turned to " << angle << " degree" << endl;
+	return angle;
 };
-void Robot::moveStraight(double distance)
+
+void Robot::setAngle(double n_angle)
 {
-  cout << "Robot moved straight to " << distance << endl;
+	angle = n_angle;
+};
+
+double Robot::getDistance()
+{
+	return distance;
+};
+
+void Robot::setDistance(double n_distance)
+{
+	distance = n_distance;
+};
+
+EState Robot::getState()
+{
+	return state;
+};
+
+void Robot::setState(EState n_state)
+{
+	state = n_state;
+};
+
+void Robot::reverse(double n_angle)
+{	
+	setAngle(n_angle);
+	cout << "Robot turned to " << getAngle() << " degree" << endl;
+};
+
+void Robot::moveStraight(double n_distance)
+{
+	cout << "Enter distance:\n";
+	setDistance(n_distance);
+	cout << "Robot moved straight to " << getDistance() << endl;
 };
 
 void Robot::clean()
 {
-  cout << "Robot cleaned the area" << endl;
+	cout << "Robot cleaned the area" << endl;
 };
+
 int Robot::R(const string& com)
 {
-	if(com == "Reverse")
-  {
-		return 1;
-	} else if(com == "Wait_cmd")
-  {
-		return 2;
+	if (com == "Wait_cmd")
+	{
+		return WAIT_CMD;
 	}
-}
-void Robot::processEvent(const string& command, EState& state){
+	else if (com == "Reverse")
+	{
+		return REVERSE;
+	}
+	else if (com == "Clean")
+	{
+		return CLEAN;
+	}
+	else if (com == "On")
+	{
+		return ON;
+	}
+	else
+	{
+		return OFF;
+	}
+};
+
+void Robot::Wait_cmdEventProcessing(const string& cmd)
+{
+	if (cmd == "Wait_cmd") {
+		cout << "Robot is on. Enter command:\n";
+		string com;
+		cin >> com;
+		processEvent(com);
+	}
+};
+
+void Robot::processEvent(const string& command) {
 	string com;
 	cin >> com;
 	switch (R(com))
 	{
-	  case 2:
-	    Wait_cmdEventProcessing(command);
-	    break;
-    case 1:
-	    double angl;
-	    cin >> angl;
-	    Robot::reverse(angl);
-	    state = EState::Wait_cmd;
-      break;
+	case WAIT_CMD:
+		Wait_cmdEventProcessing(command);
+		break;
+	case REVERSE:
+		double angl;
+		cout << "Enter angle:\n";
+		cin >> angl;
+		reverse(angl);
+		setState(EState::Wait_cmd);
+		break;
+	case CLEAN:
+		clean();
+		setState(EState::Wait_cmd);
+		break;
+	default:
+		cout << "Incorrect command. Try again";
+		break;
 	}
-}
+};
 
 int main()
 {
-    Robot paintRobot (new NetCommandStrategy());
-   paintRobot.state = EState::Wait_cmd;
-    while (paintRobot.state == EState::Wait_cmd)
-    {
-	  string cmd = paintRobot.getCommand();
-	  paintRobot.processEvent(cmd, paintRobot.state);
+	Robot paintRobot(new ConsoleCommandStrategy());
+	paintRobot.setState(EState::Wait_cmd);
+	while (paintRobot.getState() == EState::Wait_cmd)
+	{
+		string cmd = paintRobot.getCommand();
+		paintRobot.processEvent(cmd);
 	}
-    return 0;
+	return 0;
 }
