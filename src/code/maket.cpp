1c2e17d97a4fdc1b0b02d31886f014fe819bce65
amirovmir <78919537+amirovmir@users.noreply.github.com>
2022-05-07 13:55:32 +0300

    Add NetCommandStrategy

--- src/code/maket.cpp
+++ src/code/maket.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 #include<sstream>
 #include<string>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <stdio.h>
 
 #define WAIT_CMD 1
 #define REVERSE  2
@@ -30,13 +35,36 @@ public:
 
 class NetCommandStrategy : public CommandStrategy
 {
+    int sock;
+    struct sockaddr_in addr;
+    char buf[1024];
+    int bytes_read;
+    NetCommandStrategy()
+    {
+        sock = socket(AF_INET, SOCK_DGRAM, 0);
+        addr.sin_family = AF_INET;
+        addr.sin_port = htons(3425);
+        addr.sin_addr.s_addr = htonl(INADDR_ANY);
+        if(bind(sock, (struct sockaddr *)&addr, sizeof(addr)) < 0) 
+        {
+            perror("bind");
+        }
+    }
 	virtual string getCommand() const override
 	{
 		string cmd;
-		//cmd = receive();
+		cmd = receive();
 		return cmd;
 	}
+
+    string receive()
+    {
+        bytes_read = recvfrom(sock, buf, 1024, 0, NULL, NULL);
+        buf[bytes_read] = '\0';
+        return buf;
+    };
 };
+
 
 class  OnTestCommandStrategy : public CommandStrategy
 {
@@ -119,7 +147,6 @@ void Robot::reverse(double n_angle)
 
 void Robot::moveStraight(double n_distance)
 {
-	cout << "Enter distance:\n";
 	setDistance(n_distance);
 	cout << "Robot moved straight to " << getDistance() << endl;
 };
