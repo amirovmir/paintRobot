5289963df294b6614c58280cc8a2939555bc3ad5
amirovmir <78919537+amirovmir@users.noreply.github.com>
2022-05-07 15:51:35 +0300

    Update client.cpp

--- src/code/client.cpp
+++ src/code/client.cpp
@@ -6,7 +6,6 @@
 
 int main() {
   char msg[10];
-  std::cin.getline(msg, 10, '\n');
   int sock;
   struct sockaddr_in addr;
   sock = socket(AF_INET, SOCK_DGRAM, 0);
@@ -14,7 +13,11 @@ int main() {
   addr.sin_family = AF_INET;
   addr.sin_port = htons(3425);
   addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
-  sendto(sock, msg, sizeof(msg), 0, (struct sockaddr *)&addr, sizeof(addr));
+  while (1)
+  {
+    std::cin.getline(msg, 10, '\n');
+    sendto(sock, msg, sizeof(msg), 0, (struct sockaddr *)&addr, sizeof(addr));
+  }
   close(sock);
   return 0;
 }
