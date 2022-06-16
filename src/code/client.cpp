52cc88fc81cb9fd4fd54464a6be9bdf496022670
amirovmir <78919537+amirovmir@users.noreply.github.com>
2022-06-16 20:22:26 +0300

    Update client.cpp

--- src/code/client.cpp
+++ src/code/client.cpp
@@ -2,22 +2,39 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <unistd.h>
+#include <cstring>
 #include <iostream>
 
 int main() {
-  char msg[10];
-  int sock;
-  struct sockaddr_in addr;
-  sock = socket(AF_INET, SOCK_DGRAM, 0);
+    int sock;
+    struct sockaddr_in addr;
+    sock = socket(AF_INET, SOCK_DGRAM, 0);
 
-  addr.sin_family = AF_INET;
-  addr.sin_port = htons(3425);
-  addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
-  while (1)
-  {
-    std::cin.getline(msg, 10, '\n');
-    sendto(sock, msg, sizeof(msg), 0, (struct sockaddr *)&addr, sizeof(addr));
-  }
-  close(sock);
-  return 0;
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(3425);
+    addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+
+    std::string response, x;
+    char message[10];
+    while (true) {
+        std::cout << "Enter command"<< std::endl;
+        std::cout << "Command list:\n MoveStraight, Reverse, Clean" << std::endl;
+        std::cin >> response;
+
+        if (response == "MoveStraight")
+        {
+            std::cout << "Enter distance:\n";
+            std::cin >> x;
+        }
+        if (response == "Reverse")
+        {
+            std::cout << "Enter angle:\n";
+            std::cin >> x;
+        }
+        response = response + " " + x;
+        strcpy(message, response.c_str());
+        sendto(sock, message, sizeof(message), 0, (struct sockaddr *)&addr, sizeof(addr));
+    }
+    close(sock);
+    return 0;
 }
